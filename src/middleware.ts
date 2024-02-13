7433d75e35d7ac68e40c41f3fca3f087df6b49f6
Alain369 <136309614+Alain369@users.noreply.github.com>
2024-02-13 11:04:33 +0100

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,25 +3,6 @@ import { NextResponse } from 'next/server';
 
 export default authMiddleware({
   publicRoutes: [process.env.NEXT_PUBLIC_URL + '/site'],
-  async beforeAuth(auth, req) {
-    // Vor der Authentifizierung den Benutzer zur Landingpage weiterleiten
-    const url = new URL(process.env.NEXT_PUBLIC_URL + '/site');
-
-    if (url.pathname !== '/site') {
-      return NextResponse.redirect(process.env.NEXT_PUBLIC_URL + '/site');
-    }
-  },
-  async afterAuth(auth, req) {
-    const url = new URL(req.url);
-
-    // Nach der Authentifizierung den Benutzer zur Zielroute weiterleiten
-    if (url.pathname === '/site') {
-      return NextResponse.redirect(process.env.NEXT_PUBLIC_URL + '/agency');
-    }
-
-    // Standardverhalten: Weiterleitung auf die angeforderte Seite
-    return NextResponse.next();
-  },
 });
 
 export const config = {
